b566d5ad42ee947f65bc63f7352d53c59743d950
Michael Nobile <nobility33@gmail.com>
2016-09-11 10:45:35 -0400

    Week 2 is almost done

---
+++ week_2/is_in.py
@@ -0,0 +1,11 @@
+def isIn(char, aStr):
+    '''
+    char: a single character
+    aStr: an alphabetized string
+
+    returns: True if char is in aStr; False otherwise
+    '''
+    # Your code here
+
+    else:
+        return isIn(char, aStr=)
