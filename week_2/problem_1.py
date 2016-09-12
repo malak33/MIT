d30da5b3a80b9728be70d149ed2673819e2490e4
Michael Nobile <nobility33@gmail.com>
2016-09-12 11:18:09 -0400

    Week 2 is almost done adding file for week 2

---
+++ week_2/problem_1.py
@@ -0,0 +1,51 @@
+"""
+Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment
+required by the credit card company each month.
+
+The following variables contain values as described below:
+
+balance - the outstanding balance on the credit card
+
+annualInterestRate - annual interest rate as a decimal
+
+monthlyPaymentRate - minimum monthly payment rate as a decimal
+
+For each month, calculate statements on the monthly payment and remaining balance. At the end of 12 months, print out
+the remaining balance. Be sure to print out no more than two decimal digits of accuracy - so print
+
+Remaining balance: 813.41
+instead of
+
+Remaining balance: 813.4141998135
+So your program only prints out one thing: the remaining balance at the end of the year in the format:
+
+Remaining balance: 4784.0
+A summary of the required math is found below:
+
+Monthly interest rate= (Annual interest rate) / 12.0
+Minimum monthly payment = (Minimum monthly payment rate) x (Previous balance)
+Monthly unpaid balance = (Previous balance) - (Minimum monthly payment)
+Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
+"""
+
+
+balance = 42
+annualInterestRate = 0.2
+monthlyPaymentRate = 0.04
+monthly_interest_rate = annualInterestRate / 12
+minimum_monthly_payment = monthlyPaymentRate * balance
+monthly_unpaid_balance = balance - minimum_monthly_payment
+update_balance_each_month = monthly_unpaid_balance + monthly_interest_rate * monthly_unpaid_balance
+month = 0
+
+while month < 12:
+    # debugging below
+    print(balance)
+    print(annualInterestRate)
+    print(monthly_interest_rate)
+    print(minimum_monthly_payment)
+    print(monthly_unpaid_balance)
+    print(update_balance_each_month)
+    print(month)
+    month += 1
+    print('Month {} Remaining balance: {}'.format(month, update_balance_each_month))
