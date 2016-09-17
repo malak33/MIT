6381610407e03660b3add576019625d9048cb02a
Michael Nobile <nobility33@gmail.com>
2016-09-17 18:39:45 -0400

    week 3 is on the books

---
+++ week_3/test_3.py
@@ -0,0 +1,26 @@
+listA = [1, 4, 3, 0]
+listB = ['x', 'z', 't', 'q']
+
+print(listA.sort)
+print(listA.sort())
+print(listA)
+print(listA.insert(0, 100))
+print(listA.remove(3))
+print(listA.append(7))
+print(listA)
+print(listA + listB)
+print(listB.sort())
+print(listB.pop())
+print(listB.count('a'))
+#print(listB.remove('a'))
+print(listA.extend([4, 1, 6, 3, 4]))
+print(listA.count(4))
+print(listA.index(1))
+print(listA.pop(4))
+print(listA.reverse())
+print(listA)
+
+
+
+
+
---
+++ week_3/test_5.py
@@ -0,0 +1,7 @@
+testList = [1, -4, 8, -9]
+
+def applyToEach(L, f):
+    for i in range(len(L)):
+        L[i] = f(L[i])
+
+applyToEach(    )
